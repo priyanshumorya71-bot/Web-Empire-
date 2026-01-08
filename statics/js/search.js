4ee952ceec0bc43e6bc2681660dbbb7631bbd810
King wholesaler <priyanshumorya71@gmail.com>
2026-01-08 17:01:45 +0530

    Update search.js

--- statics/js/search.js
+++ statics/js/search.js
@@ -1,1 +1,24 @@
+const box = document.getElementById("searchBox");
+const suggestions = document.getElementById("suggestions");
 
+box.addEventListener("input", async () => {
+    const q = box.value;
+    if (q.length < 2) {
+        suggestions.innerHTML = "";
+        return;
+    }
+
+    const res = await fetch(`/search?q=${q}`);
+    const data = await res.json();
+
+    suggestions.innerHTML = "";
+    data.forEach(item => {
+        suggestions.innerHTML += `
+          <div class="result">
+            <a href="${item.url}" target="_blank">
+              ${item.name} (${item.category})
+            </a>
+          </div>
+        `;
+    });
+});
